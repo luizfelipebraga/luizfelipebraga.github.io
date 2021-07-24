ad870fd3a6f0f9da07b4b57121485c0d7e3d4244
Luiz <ruivolfb@gmail.com>
2021-07-24 20:58:51 -0300

    responsividade

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const skillSection = document.getElementById('skills');
+// const skillSection = document.getElementById('skills');
 
-const progressBar = document.querySelectorAll('progress-line')
+// const progressBar = document.querySelectorAll('progress-line')
 
 
-function showProgress() {
-    progressBar.forEach(progressBar => {
-        const value = progressBar.dataset.progress;
-        progressBar.style.opacity = 1;
-        progressBar.style.width = `${value}`;
-    });
-}
+// function showProgress() {
+//     progressBar.forEach(progressBar => {
+//         const value = progressBar.dataset.progress;
+//         progressBar.style.opacity = 1;
+//         progressBar.style.width = `${value}`;
+//     });
+// }
 
-function hideProgress() {
-    progressBar.forEach(progressBar => { 
-        progressBar.style.opacity = 0;
-        progressBar.style.width = 0;
-    });
-}
+// function hideProgress() {
+//     progressBar.forEach(progressBar => { 
+//         progressBar.style.opacity = 0;
+//         progressBar.style.width = 0;
+//     });
+// }
 
-window.addEventListener('scroll', () => {
-    skillSection.getBoundingClientRect() === null ? null : skillSection.getBoundingClientRect()
-    const sectionPos = skillSection.getBoundingClientRect().top;
-    sectionPos !== null? sectionPos : null;
-    const screenPos = window.innerHeight;
+// window.addEventListener('scroll', () => {
+//     skillSection.getBoundingClientRect() === null ? null : skillSection.getBoundingClientRect()
+//     const sectionPos = skillSection.getBoundingClientRect().top;
+//     sectionPos !== null? sectionPos : null;
+//     const screenPos = window.innerHeight;
 
-    if(sectionPos < screenPos) {
-        showProgress();
-    }else {
-        hideProgress();
-    }
-})
+//     if(sectionPos < screenPos) {
+//         showProgress();
+//     }else {
+//         hideProgress();
+//     }
+// })
