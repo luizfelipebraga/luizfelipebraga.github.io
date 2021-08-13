18013130f430541430c53c4c625e6a64c34951d5
Luiz <ruivolfb@gmail.com>
2021-08-13 09:45:49 -0300

    add neon on my name and fix the responsive

--- js/index.js
+++ js/index.js
@@ -1,24 +1,14 @@
 const skillSection = document.getElementById('skills');
 
-const progressBar = document.querySelectorAll('.progress-line')
-
 
 function showProgress() {
     skillSection.style.opacity = 1;
-    skillSection.style.transition = opacity; 1;
-
-    progressBar.forEach(progress => {
-        progress.style.opacity = 1;
-        progress.style.with = 1;
-    })
+    skillSection.style.transition = 1;
 }
 
 function hideProgress() {
     skillSection.style.opacity = 0;
-    // progressBar.forEach(progress => {
-    //     progress.style.opacity = 0;
-    //     progress.style.with = 0;
-    // })
+    skillSection.style.transition = 0;
 }
 
 window.addEventListener('scroll', () => {
