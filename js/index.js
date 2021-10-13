498967ea16a58481468fdbbc3e7e226c6df09b51
luizfelipebraga <ruivolfb@gmail.com>
2021-10-13 16:26:28 -0300

    style(js): cleaning the code on js path

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ let lastScroll = 0;
 window.addEventListener("scroll", () => {
 	const currentScroll = window.pageYOffset;
     nav.classList.toggle("sticky", window.scrollY > 600);
+	
 	if (currentScroll <= 0) {
 		body.classList.remove("scroll-up");
 		return;
@@ -17,12 +18,12 @@ window.addEventListener("scroll", () => {
 	if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
 		body.classList.remove("scroll-up");
 		body.classList.add("scroll-down");
-	} else if (
-		currentScroll < lastScroll &&
-		body.classList.contains("scroll-down")
-	) {
+	} 
+	
+	else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
 		body.classList.remove("scroll-down");
 		body.classList.add("scroll-up");
 	}
+
 	lastScroll = currentScroll;
 });
